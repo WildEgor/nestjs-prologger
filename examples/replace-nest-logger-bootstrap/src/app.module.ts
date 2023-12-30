7e794dccf67e8b0100d2cf8419b3e352e8d632c9
Egor Kartashov <kartashov_egor96@mail.ru>
2023-12-31 01:48:22 +0600

    fix: fix

--- examples/replace-nest-logger-bootstrap/src/app.module.ts
+++ examples/replace-nest-logger-bootstrap/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from '@wildegor/nestjs-prologger';
+import { ConfigModule } from './infrastructure/configs/config.module';
+import { LoggerConfig } from './infrastructure/configs/logger.config';
 
 @Module({
   imports: [
-
+    ConfigModule,
+    LoggerModule.forRootAsync({
+      useExisting: LoggerConfig,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, Logger],
--- examples/replace-nest-logger-bootstrap/src/main.ts
+++ examples/replace-nest-logger-bootstrap/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerConstants } from '@wildegor/nestjs-prologger/modules/modules/logger/logger.constants';
+import { LoggerAdapter } from '@wildegor/nestjs-prologger/modules/modules/logger/logger.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: , // TODO
   });
+
+  const logger = app.get<LoggerAdapter>(LoggerConstants.logger);
+
+  logger.log('TEST');
+
   await app.listen(3000);
 }
 bootstrap();
